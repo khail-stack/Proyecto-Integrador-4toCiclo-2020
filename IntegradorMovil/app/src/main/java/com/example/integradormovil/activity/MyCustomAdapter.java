78c995a01c3f95067147685f40f2ae0b806eca9f
JonathanHuarca <jonathan.huarca@tecsup.edu.pe>
2020-07-08 00:57:07 -0500

    Ejemplo de App de Coronavirus

---
+++ IntegradorMovil/app/src/main/java/com/example/integradormovil/activity/MyCustomAdapter.java
@@ -0,0 +1,13 @@
+package com.example.integradormovil.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyCustomAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_custom_adapter);
+    }
+}
---
+++ IntegradorMovil/app/src/main/java/com/example/integradormovil/models/CountryModel.java
@@ -0,0 +1,4 @@
+package com.example.integradormovil.models;
+
+public class CountryModel {
+}
